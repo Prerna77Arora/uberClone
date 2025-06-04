30b13baa9ed9df797c49c011a2bfffdb493ecca6
Prerna77Arora <prerna7788arora@gmail.com>
2025-06-04 19:10:08 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const dotenve = require('dotenv');
+dotenve.config();
+const express = require('express');
+const cors = require('cors');
+const app = express()
+app.use(cors());
+
+app.get('/',(req,res) => {
+      res.send("hi");
+});
+
+module.exports = app;
